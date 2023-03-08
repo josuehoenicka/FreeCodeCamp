2a385aafd85655229985ec16f88aa7faa9899313
Josue Hoenicka <95772739+josuehoenicka@users.noreply.github.com>
2023-03-08 16:16:37 -0300

    update: add/finish repeatAStringRepeatAString.js

---
+++ technologies/javascript/repeatAStringRepeatAString.js
@@ -0,0 +1,15 @@
+// Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
+
+function repeatStringNumTimes(str, num) {
+    let result = str;
+    if (num > 0) {
+        for(let i = 1; i < num; i++){
+        result += str;
+        }
+    } else {
+        result = ""
+    }
+    return result;
+}
+
+console.log(repeatStringNumTimes("abc", 3));
