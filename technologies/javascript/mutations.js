71dfcdefd39a1760ba0f8d8205baa109ecf8c638
Josue Hoenicka <95772739+josuehoenicka@users.noreply.github.com>
2023-03-11 23:44:57 -0300

    update: finish mutation.js

---
+++ technologies/javascript/mutations.js
@@ -0,0 +1,17 @@
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+// The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+// Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+function mutation(arr) {
+    const str1 = arr[0].toLowerCase();
+    const str2 = arr[1].toLowerCase();
+    for (let i = 0; i < str2.length; i++) {
+      if (str1.indexOf(str2[i]) === -1) {
+        return false;
+      }
+    }
+    return true;
+}
+
+console.log(mutation(["hello", "hey"])); // false
